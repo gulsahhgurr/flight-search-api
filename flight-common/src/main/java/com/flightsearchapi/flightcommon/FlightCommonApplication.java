6a0cc2046bdc1ff9d6b6abc2bf3adedbc2579989
gulsahhgurr <glsh.cifci@gmail.com>
2026-01-04 13:13:35 +0300

    feat: project modules created.

--- flight-common/src/main/java/com/flightsearchapi/flightcommon/FlightCommonApplication.java
+++ flight-common/src/main/java/com/flightsearchapi/flightcommon/FlightCommonApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlightCommonApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(FlightCommonApplication.class, args);
     }
