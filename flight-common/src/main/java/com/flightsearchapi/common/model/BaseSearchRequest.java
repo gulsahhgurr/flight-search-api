b0783705fbd052c627598cfa9b81b95aae552439
gulsahhgurr <glsh.cifci@gmail.com>
2026-01-09 11:16:46 +0300

    fix: fields updated.

--- flight-common/src/main/java/com/flightsearchapi/common/model/BaseSearchRequest.java
+++ flight-common/src/main/java/com/flightsearchapi/common/model/BaseSearchRequest.java
@@ -4,7 +4,25 @@ import java.time.LocalDateTime;
 
 public class BaseSearchRequest {
 
+    private String departure;
+    private String arrival;
     private LocalDateTime departureDate;
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
 
     public LocalDateTime getDepartureDate() {
         return departureDate;
